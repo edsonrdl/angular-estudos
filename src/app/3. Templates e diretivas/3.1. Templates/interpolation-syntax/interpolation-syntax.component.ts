26b774f5f57929a8f42d887d4d281e72085ffe97
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2025-02-09 15:36:33 -0300

    criado interpolação sintaxe

---
+++ src/app/3. Templates e diretivas/3.1. Templates/interpolation-syntax/interpolation-syntax.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation-syntax',
+  imports: [],
+  standalone: true,
+  templateUrl: './interpolation-syntax.component.html',
+  styleUrl: './interpolation-syntax.component.scss'
+})
+export class InterpolationSyntaxComponent {
+  technology : string  = 'Angular';
+  areaTechnology : string = 'Framwork front-end';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 
 import { Component } from '@angular/core';
-import { ParentComponent } from './2. Componentes/2.2. Interação entre componentes/2.2.2. viewChild e ContentChild/contentChild/parent/parent.component';
+import { InterpolationSyntaxComponent } from './3. Templates e diretivas/3.1. Templates/interpolation-syntax/interpolation-syntax.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [ParentComponent]
+  imports: [InterpolationSyntaxComponent]
 })
 export class AppComponent {
   title = 'angular-estudos';
