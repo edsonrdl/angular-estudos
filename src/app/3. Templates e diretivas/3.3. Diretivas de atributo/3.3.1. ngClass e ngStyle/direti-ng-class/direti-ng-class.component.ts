cab58e3175f8cd1e26cbe8ce1d63838a9d7dec8a
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2025-02-10 22:38:50 -0300

    feat:criado diretiva NgClass

--- src/app/3. Templates e diretivas/3.3. Diretivas de atributo/3.3.1. ngClass e ngStyle/direti-ng-class/direti-ng-class.component.ts
+++ src/app/3. Templates e diretivas/3.3. Diretivas de atributo/3.3.1. ngClass e ngStyle/direti-ng-class/direti-ng-class.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-direti-ng-class',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './direti-ng-class.component.html',
   styleUrl: './direti-ng-class.component.scss'
 })
 export class DiretiNgClassComponent {
+  collor : string = 'red';
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@
 import { Component } from '@angular/core';
 import { DiretivaNgSwitchComponent } from "./3. Templates e diretivas/3.2. Diretivas/3.2.3. ngSwitch/diretiva-ng-switch/diretiva-ng-switch.component";
 import { DiretiNgStyleComponent } from "./3. Templates e diretivas/3.3. Diretivas de atributo/3.3.1. ngClass e ngStyle/direti-ng-style/direti-ng-style.component";
+import { DiretiNgClassComponent } from "./3. Templates e diretivas/3.3. Diretivas de atributo/3.3.1. ngClass e ngStyle/direti-ng-class/direti-ng-class.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [DiretivaNgSwitchComponent, DiretiNgStyleComponent],
+  imports: [DiretivaNgSwitchComponent, DiretiNgStyleComponent, DiretiNgClassComponent],
 })
 export class AppComponent {
   title = 'angular-estudos';
