e079b58a4a6d24cd07a4267c650494ccb6b5ce29
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2025-05-04 23:19:48 -0300

    corrigido implementacao de router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,16 @@ import { DiretiNgStyleComponent } from "./3. Templates e diretivas/3.3. Diretiva
 import { DiretiNgClassComponent } from "./3. Templates e diretivas/3.3. Diretivas de atributo/3.3.1. ngClass e ngStyle/direti-ng-class/direti-ng-class.component";
 import { FormTecComponent } from "./7. Formularios/7.1. Formularios Reativos/7.1.1. FormGroup e FormControl/form-tec/form-tec.component";
 import { ParentComponent } from './2. Componentes/2.3. Metodos de ciclo de vida de componentes/2.3.1. ngOnDestroy/parent/parent.component';
+import { PropertyBindingComponent } from './3. Templates e diretivas/3.1. Templates/3.1.2. Bindings  event e property/property-binding/property-binding.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [DiretivaNgSwitchComponent, DiretiNgStyleComponent, DiretiNgClassComponent, FormTecComponent, ParentComponent],
+  imports: [RouterModule, PropertyBindingComponent,DiretivaNgSwitchComponent, DiretiNgStyleComponent, DiretiNgClassComponent, FormTecComponent, ParentComponent],
 })
 export class AppComponent {
   title = 'angular-estudos';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PropertyBindingComponent } from './3. Templates e diretivas/3.1. Templates/3.1.2. Bindings  event e property/property-binding/property-binding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: PropertyBindingComponent },
+];
