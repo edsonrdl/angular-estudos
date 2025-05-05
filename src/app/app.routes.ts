81a001411ae498e04b1f52cf75bef8660e79c5a9
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2025-05-04 23:23:20 -0300

    refactor: Corrigido nome da rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { PropertyBindingComponent } from './3. Templates e diretivas/3.1. Templates/3.1.2. Bindings  event e property/property-binding/property-binding.component';
 
 export const routes: Routes = [
-  { path: 'home', component: PropertyBindingComponent },
+  { path: 'PropertyBindingComponent', component: PropertyBindingComponent },
 ];
